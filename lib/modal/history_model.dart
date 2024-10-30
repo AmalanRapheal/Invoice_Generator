6621daac109e44c172e98aec56100bbfb2893546
AmalanRapheal <amalanrapheal@gmail.com>
2024-10-30 14:34:43 +0530

    Deleted the project

--- lib/modal/history_model.dart
+++
@@ -1,8 +0,0 @@
-class HistoryModel {
-  final String title;
-  final String date;
-  final String amount;
-
-  const HistoryModel(
-      {required this.title, required this.date, required this.amount});
-}
